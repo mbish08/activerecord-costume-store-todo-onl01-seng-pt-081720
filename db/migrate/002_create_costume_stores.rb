9abc020c300c3d4fcb635efb1e40580a6079bc09
Mikel Bishop <mdrew8@gmail.com>
2020-11-10 02:06:17 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,3 +1,16 @@
 # Create your costume_stores migration here
 
-class 
+class CreateCostumeStores < ActiveRecord::Migration[5.2]
+  
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory 
+      t.boolean :
+  end 
+  
+  
+  
+  
+end 
